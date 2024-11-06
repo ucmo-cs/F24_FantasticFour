49f6c10e8e40c73561961cfa191889ce116d394f
SS <sxs29640@ucmo.edu>
2024-11-06 10:56:44 -0600

    merge

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import AdminDash from './Pages/AdminDash';
 import LoanCreator from './Pages/LoanCreator'
 import Login from './Pages/Login'
 import SpecificLoan from './Pages/SpecificLoan';
+import Customer from "./Pages/Customer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/admindash" exact={true} element={<AdminDash/>}/>
       <Route path="/loancreator" exact={true} element={<LoanCreator/>}/>
       <Route path="/loan/:loanid" element={<SpecificLoan/>}/>
+      <Route path="/customer" element={<Customer/>}/>
     </Routes>
   </div>
   );
